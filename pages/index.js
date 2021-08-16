61f890dbbf5380bb521d6d3bdea6fc60c72afe30
Moreno Andrade <laforetmoreno@gmail.com>
2021-08-16 12:33:58 -0300

    Added sort filter

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+
+import dynamic from 'next/dynamic';
+
+const BarcodeScannerComponent = dynamic(
+  () => import('react-webcam-barcode-scanner'),
+  {
+    ssr: false,
+  }
+);
+
+// import BarcodeScannerComponent from 'react-webcam-barcode-scanner';
 
 export default function Home() {
+  const [data, setData] = React.useState('Not Found');
+  const [error, setError] = React.useState(null);
+
+  const handleScan = data => {
+    setData(data);
+  };
+  const onError = err => setError(err);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <>
+        <BarcodeScannerComponent
+          width={500}
+          height={500}
+          onUpdate={(err, result) => {
+            if (result) {
+              console.log(result, 'result');
+              setData(result.text);
+            } else setData('Not Found');
+          }}
+        />
+        <p>{data}</p>
+      </>
     </div>
-  )
+  );
 }
